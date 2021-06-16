0fd385f0bf69078ce69c8e0cbe9363327d44dab4
Den <73191251+DenisShell@users.noreply.github.com>
2021-06-16 17:30:44 +0700

    Add files via upload
    Tickets aand age

---
+++ Conf_task_13.8.19.py
@@ -0,0 +1,15 @@
+tickets  = int(input('Колличество билетов:'))
+result = 0
+
+for person in range(tickets):
+    age = int(input('Возраст участникa:'))
+    if age < 18:
+        result += 0
+    elif 18 <= age < 25:
+        result += 990
+    elif age >= 25:
+        result += 1390
+if tickets >= 3:
+    result = result - (result * 0.1)
+print('Полная стоимость билетов на конференцию с учетом скидок:', result)
+
